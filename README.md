0a2606a5e06c6ccd56d4bcc9580e32701e08b43a
Alexey Poryadin <alexey.poryadin@gmail.com>
2016-11-16 19:32:49 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Squall #
+
+The Squall is a framework which implements the cooperative multitasking
+based on event-driven switching async/await coroutines.
---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from setuptools import setup
+
+if sys.version_info[:2] < (3, 5):
+    raise NotImplementedError("Required python version 3.5 or greater")
+
+settings = {
+    'name': 'squall',
+    'version': '0.2.dev0',
+    'author': "Alexey Poryadin",
+    'author_email': "alexey.poryadin@gmail.com",
+    'description': "The Squall is a framework which implements the cooperative "
+                   "multitasking based on event-driven switching async/await "
+                   "coroutines.",
+}
+
+setup(**settings)
